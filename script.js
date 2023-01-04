b7554001ecb43c276adc49f580b48bf60d8dea1f
MarcosGTS <marcosgtsodre@gmail.com>
2023-01-04 13:20:12 -0300

    add basic redering

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 class Tree {
     
-    constructor(position, height, brach_length, spread_degree) {
+    constructor(position, height, brach_length, orientation) {
+        this.orientation = orientation
         this.position = position
         this.height = height
         this.brach_length = brach_length
-        this.spread_degree = spread_degree
+        this.spread_degree = 1
 
         this.branches = []
     }
@@ -19,11 +20,12 @@ class Tree {
         if (height <= 0) return 
 
         for (let i = 0; i < number_branches; i++) {
-            const new_x = position[0] + branch_length * Math.cos(i * spread_degree)
-            const new_y = position[1] + branch_length * Math.sin(i * spread_degree)
+            const new_orientation = i * spread_degree + this.orientation * 0.75
+            const new_x = position[0] + branch_length * Math.cos(new_orientation)
+            const new_y = position[1] + branch_length * Math.sin(new_orientation)
             const new_position = [new_x, new_y]
 
-            let new_branch = new Tree(new_position, height - 1, branch_length / 2, spread_degree)
+            let new_branch = new Tree(new_position, height - 1, branch_length / 1.2, new_orientation)
 
             this.branches.push(new_branch)     
         }
@@ -35,7 +37,6 @@ class Tree {
     }
 
     get_tree_points() {
-        console.log(this.branches.length)
         
         if (this.branches.length == 0) {
             return []
@@ -53,13 +54,21 @@ class Tree {
     }
 }
 
-function Render (tree) {
-    // tree -> List of points that compose the tree
-    console.log(tree)
+function Render (context, tree) {
+    
+    for (let [p1, p2] of tree) {
+        context.beginPath()
+        context.moveTo (p1[0], p1[1]);   context.lineTo (p2[0], p2[1]);  
+        context.stroke()
+    }
+    
 }
 
-new_tree = new Tree([0,0], 3, 10, Math.PI)
-
+const canvas = document.getElementById("canvas")
+const context = canvas.getContext('2d')
+canvas.width = 500
+canvas.height = 500
+new_tree = new Tree([250, 250], 3, 50, 3.1415 * 3/2)
 new_tree.create(2)
 
-Render(new_tree.get_tree_points())
+Render(context, new_tree.get_tree_points())
